3bb1c1974d006370280374f65641a4d2995adc56
saad <saad-mogren@hotmail.com>
2021-04-16 04:42:15 +0300

    display owner details

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 //add prefix
 @RequestMapping("/owners")
@@ -26,5 +30,12 @@ public class OwnerController {
     @RequestMapping({ "/find"})
     public String findOwners(Model model){
         return "notimplemented";
+    }
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 }
